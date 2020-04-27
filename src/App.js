6eff0faabde6f8ad53d3720ee7a610e592795c6d
dongeun <kin911014@daum.net>
2020-04-27 17:03:39 +0900

    Learn Components with JSX and Props part two

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ function App() {
       <Food 
         fav="galic" 
       />
+    {/* Food는 컴포넌트로 시작은 대문자, fav는 property, galic은 value이며
+    function Food ({fav}){}는  value값을 바로 argument에 대입하는 방법*/}
     </div>
   )
 }
