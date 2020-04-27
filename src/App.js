3ed150cb1acb2571d09744524a62259e1c752464
dongeun <kin911014@daum.net>
2020-04-27 17:01:36 +0900

    Learn Components with JSX and Props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({fav}){
+return <h1>I hate {fav}</h1>
+}
 
 function App() {
   return (
     <div>
       <h1>hello !</h1>
-      <Potato />
-      </div>
+      <Food 
+        fav="kimchi" 
+      />
+      <Food 
+        fav="ramen" 
+      />
+      <Food 
+        fav="gulbi" 
+      />
+      <Food 
+        fav="galic" 
+      />
+    </div>
   )
 }
 
--- src/Potato.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Potato(){
-    return (
-        <h3>Potato</h3>
-    )
-}
-
-export default Potato;
